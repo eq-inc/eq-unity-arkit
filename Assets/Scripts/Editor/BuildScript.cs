3793989b2910a7ef5ce3a1e9f1ea4e7936bf297f
辰口寿志 <tatsuguchi@eq-inc.jp>
2018-02-16 08:09:50 +0900

    BuildScriptに名前空間を追加

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -3,32 +3,34 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class BuildScript{
-	public static void Build(){
-		string[] scenes = {
-			"./Assets/Scenes/Top.unity",
-            "./Assets/UnityARKitPlugin/Examples/AddRemoveAnchorExample/AddRemoveAnchorScene.unity",
-            "./Assets/UnityARKitPlugin/Examples/FaceTracking/FaceAnchorScene.unity",
-            "./Assets/UnityARKitPlugin/Examples/FaceTracking/FaceBlendshapeScene.unity",
-            "./Assets/UnityARKitPlugin/Examples/FaceTracking/FaceBlendshapeSloth.unity",
-            "./Assets/UnityARKitPlugin/Examples/FaceTracking/FaceDirectionalLightEstimate.unity",
-            "./Assets/UnityARKitPlugin/Examples/FaceTracking/FaceMeshScene.unity",
-            "./Assets/UnityARKitPlugin/Examples/FocusSquare/FocusSquareScene.unity",
-            "./Assets/UnityARKitPlugin/Examples/UnityARBallz/UnityARBallz.unity",
-            "./Assets/UnityARKitPlugin/Examples/UnityARKitScene/UnityARKitScene.unity",
-            "./Assets/UnityARKitPlugin/Examples/UnityAROcclusion/UnityAROcclusion.unity",
-            "./Assets/UnityARKitPlugin/Examples/UnityARShadows/UnityARShadows.unity",
-            "./Assets/UnityARKitPlugin/Examples/UnityParticlePainter/UnityParticlePainter.unity",
-        };
+namespace EQ{
+	public class BuildScript{
+		public static void Build(){
+			string[] scenes = {
+				"./Assets/Scenes/Top.unity",
+				"./Assets/UnityARKitPlugin/Examples/AddRemoveAnchorExample/AddRemoveAnchorScene.unity",
+				"./Assets/UnityARKitPlugin/Examples/FaceTracking/FaceAnchorScene.unity",
+				"./Assets/UnityARKitPlugin/Examples/FaceTracking/FaceBlendshapeScene.unity",
+				"./Assets/UnityARKitPlugin/Examples/FaceTracking/FaceBlendshapeSloth.unity",
+				"./Assets/UnityARKitPlugin/Examples/FaceTracking/FaceDirectionalLightEstimate.unity",
+				"./Assets/UnityARKitPlugin/Examples/FaceTracking/FaceMeshScene.unity",
+				"./Assets/UnityARKitPlugin/Examples/FocusSquare/FocusSquareScene.unity",
+				"./Assets/UnityARKitPlugin/Examples/UnityARBallz/UnityARBallz.unity",
+				"./Assets/UnityARKitPlugin/Examples/UnityARKitScene/UnityARKitScene.unity",
+				"./Assets/UnityARKitPlugin/Examples/UnityAROcclusion/UnityAROcclusion.unity",
+				"./Assets/UnityARKitPlugin/Examples/UnityARShadows/UnityARShadows.unity",
+				"./Assets/UnityARKitPlugin/Examples/UnityParticlePainter/UnityParticlePainter.unity",
+			};
 
-        string errorMessage = BuildPipeline.BuildPlayer(scenes, "./xcode", BuildTarget.iOS, BuildOptions.Development);
-        if (!string.IsNullOrEmpty(errorMessage))
-        {
-            Debug.LogError("[Error!] " + errorMessage);
-        }
-        else
-        {
-            Debug.Log("[Success!]");
-        }
-    }
+			string errorMessage = BuildPipeline.BuildPlayer(scenes, "./xcode", BuildTarget.iOS, BuildOptions.None);
+			if (!string.IsNullOrEmpty(errorMessage))
+			{
+				Debug.LogError("[Error!] " + errorMessage);
+			}
+			else
+			{
+				Debug.Log("[Success!]");
+			}
+		}
+	}
 }
