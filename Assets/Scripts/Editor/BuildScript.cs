b0affad84076d637611c73514d1a602a6b52f24a
辰口寿志 <tatsuguchi@eq-inc.jp>
2018-02-15 09:50:50 +0900

    一時保存

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildScript{
+	public static void Build(){
+		string[] scenes = {
+			"Assets/Scenes/Top.unity",
+			"Assets/Scenes/Top.unity",
+		};
+	}
+}
